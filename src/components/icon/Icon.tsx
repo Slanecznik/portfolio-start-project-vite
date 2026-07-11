edd9b74fbe980e3a3c6a7934895175fab55f820d
vovas <vova.shigaev121183@gmail.com>
2026-07-11 03:04:31 +0200

    1.4

--- src/components/icon/Icon.tsx
+++ src/components/icon/Icon.tsx
@@ -2,18 +2,20 @@ import iconsSprite from "../../assets/images/icons-sprite.svg";
 
 type IconPropsType = {
     iconId: string;
+    width?: string;
+    height?: string;
+    viewBox?: string;
 };
 
-export const Icon = ({ iconId }: IconPropsType) => {
-    const url = `${iconsSprite}#${iconId}`;
-
-    console.log("iconsSprite =", iconsSprite);
-    console.log("iconId =", iconId);
-    console.log("url =", url);
-
+export const Icon = ({
+                         iconId,
+                         width = "50",
+                         height = "50",
+                         viewBox = "0 0 24 24",
+                     }: IconPropsType) => {
     return (
-        <svg width="50" height="50" viewBox="0 0 24 24">
-            <use href={url} />
+        <svg width={width} height={height} viewBox={viewBox}>
+            <use href={`${iconsSprite}#${iconId}`} />
         </svg>
     );
 };
---
+++ src/layout/sections/skills/Skills.tsx
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+import {FlexWrapper} from "../../../components/FlexWrapper.tsx";
+import {Icon} from "../../../components/icon/Icon.tsx";
+import {SectionTitle} from "../../../components/SectionTitle.tsx";
+import {Skill} from "./skill/Skill.tsx";
+
+
+export const Skills = () => {
+    return (
+        <StyledSkills>
+            <SectionTitle>My Skills</SectionTitle>
+            <FlexWrapper wrap={"wrap"} justify="space-between">
+               <Skill iconId={"css"}
+                      title={"HTML5"}
+                      description={"fvfvveveeev"}/>
+                <Skill iconId={"css"}
+                      title={"HTML5"}
+                      description={"fvfvveveeev"}/>
+                <Skill iconId={"css"}
+                      title={"HTML5"}
+                      description={"fvfvveveeev"}/>
+                <Skill iconId={"css"}
+                      title={"HTML5"}
+                      description={"fvfvveveeev"}/>
+                <Skill iconId={"css"}
+                      title={"HTML5"}
+                      description={"fvfvveveeev"}/>
+                <Skill iconId={"css"}
+                      title={"HTML5"}
+                      description={"fvfvveveeev"}/>
+
+            </FlexWrapper>
+        </StyledSkills>
+    );
+};
+
+const StyledSkills = styled.section`
+    background-color: #27e811;
+    min-height: 100vh;
+`
