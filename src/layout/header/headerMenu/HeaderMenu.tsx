f3d2d2f1f6838fb8b19f2e05cb97c9463833e713
vovas <vova.shigaev121183@gmail.com>
2026-07-22 03:36:54 +0200

    1.9

--- src/layout/header/headerMenu/HeaderMenu.tsx
+++ src/layout/header/headerMenu/HeaderMenu.tsx
@@ -1,13 +1,21 @@
 import styled from "styled-components";
+import {theme} from "../../../styles/Theme.ts";
 
 export const HeaderMenu = (props: { menuItems: Array<string> }) => {
     return (
         <StyledHeaderMenu>
             <ul>
                 {props.menuItems.map((item, index) => {
-                    return (<li key={index}>
-                        <a href="">{item}</a>
-                    </li>)
+                    return (<ListItem key={index}>
+                        <Link href="">{item}
+                            <Mask>
+                                <span>{item}</span>
+                            </Mask>
+                            <Mask>
+                                <span>{item}</span>
+                            </Mask>
+                        </Link>
+                    </ListItem>)
                 })}
             </ul>
         </StyledHeaderMenu>
@@ -19,5 +27,36 @@ const StyledHeaderMenu = styled.nav`
         display: flex;
         gap: 30px;
         justify-content: center;
+    }
+`
+const ListItem = styled.li`
+    position: relative;
+
+`
+const Link = styled.a`
+    font-family: Josefin Sans, sans-serif;
+    font-weight: 400;
+    font-size: 30px;
+    text-align: center;
+
+
+`
+const Mask = styled.span`
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: inline-block;
+    height: 50%;
+    overflow-y: hidden;
+    outline: 1px solid red;
+    color: ${(theme.colors.accentBg)};
+
+    & + & {
+        top: 50%;
+
+        span {
+            display: inline-block;
+            transform: translateY(-50%);
+        }
     }
 `
